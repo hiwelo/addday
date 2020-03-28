9ee9b71b5f4f27f5ad4eb0e10c1ba49838684def
Damien Senger <damien.senger@castoredc.com>
2020-03-28 16:42:46 +0100

    feat: install and setup react-navigation with first test view

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { dark as darkTheme, mapping } from '@eva-design/eva';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ApplicationProvider } from '@ui-kitten/components';
 import React from 'react';
-import { Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import * as Sentry from 'sentry-expo';
 
 import sentryConfig from './src/config/sentry';
 import { getPersistor, getStore } from './src/data/store';
+import WelcomeScreen from './src/views/WelcomeScreen';
 
 const App: React.FC = () => {
   /** Fetches the persistor and the store */
@@ -17,11 +19,18 @@ const App: React.FC = () => {
   /** Initializes Sentry */
   Sentry.init(sentryConfig);
 
+  /** Initializes the navigation and routing system */
+  const { Navigator, Screen } = createStackNavigator();
+
   return (
     <ApplicationProvider mapping={mapping} theme={darkTheme}>
       <Provider store={store}>
         <PersistGate persistor={persistor as any}>
-          <Text>Hello World!</Text>
+          <NavigationContainer>
+            <Navigator>
+              <Screen component={WelcomeScreen} name="Welcome" />
+            </Navigator>
+          </NavigationContainer>
         </PersistGate>
       </Provider>
     </ApplicationProvider>
---
+++ src/views/WelcomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const WelcomeScreen: React.FC = () => {
+  return <Text>Hello World!</Text>;
+};
+
+export default WelcomeScreen;
