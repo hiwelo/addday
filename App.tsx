c86907122404fb5b1f11d4e8b7595a058c88a4b6
Damien Senger <damien.senger@castoredc.com>
2020-03-28 11:52:10 +0100

    Add UIKitten to App

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { dark as darkTheme, mapping } from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 import React from 'react';
 import { Text } from 'react-native';
 import { Provider } from 'react-redux';
@@ -6,11 +8,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './src/data';
 
 const App: React.FC = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Text>Hello World!</Text>
-    </PersistGate>
-  </Provider>
+  <ApplicationProvider mapping={mapping} theme={darkTheme}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Text>Hello World!</Text>
+      </PersistGate>
+    </Provider>
+  </ApplicationProvider>
 );
 
 export default App;
