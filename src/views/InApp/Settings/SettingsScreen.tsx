12d2784e85858a6b3bb3eb3ef9953c0d98d9494f
Damien Senger <damien.senger@castoredc.com>
2020-04-11 01:04:54 +0200

    feat: add sentry state in settings

--- src/views/InApp/Settings/SettingsScreen.tsx
+++ src/views/InApp/Settings/SettingsScreen.tsx
@@ -1,16 +1,31 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button } from '@ui-kitten/components';
+import { Button, Toggle } from '@ui-kitten/components';
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 
+import ScrollableContainer from '../../../components/ScrollableContainer';
 import Space from '../../../components/Space';
 import ViewLayout from '../../../components/ViewLayout';
+import { setSentryEnrollment } from '../../../data/Config/actions';
 import { setUser } from '../../../data/User/actions';
+import { useI18n } from '../../../services/LocalizationProvider';
 
 const SettingsScreen: React.FC = () => {
   const dispatch = useDispatch();
+  const { __ } = useI18n();
   const { navigate } = useNavigation();
+  const { getState } = useStore();
+  const { config } = getState();
+  const [sentryEnrollment, updateSentryEnrollment] = React.useState(
+    config.sentryEnrollment,
+  );
 
+  /** This effect aims to update the sentry enrollment in the store */
+  React.useEffect(() => {
+    dispatch(setSentryEnrollment(sentryEnrollment));
+  }, [dispatch, sentryEnrollment]);
+
+  /** This callback is fired to reset the account and loads again the wizard */
   const resetAccount = () => {
     dispatch(
       setUser({
@@ -24,9 +39,18 @@ const SettingsScreen: React.FC = () => {
 
   return (
     <ViewLayout headerTitle="Settings">
-      <Space>
-        <Button onPress={resetAccount}>Reset account</Button>
-      </Space>
+      <ScrollableContainer>
+        <Space type="comfortable">
+          <Toggle
+            checked={sentryEnrollment}
+            onChange={updateSentryEnrollment}
+            text={__('crashDataAuthScreen.toggle')}
+          />
+        </Space>
+        <Space>
+          <Button onPress={resetAccount}>Reset account</Button>
+        </Space>
+      </ScrollableContainer>
     </ViewLayout>
   );
 };
