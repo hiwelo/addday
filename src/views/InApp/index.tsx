026d2a4317338a97102653a8d5273bd18e06c493
Damien Senger <damien.senger@castoredc.com>
2020-04-11 23:28:58 +0200

    feat: use LocalAuthentication to protect the application

--- src/views/InApp/index.tsx
+++ src/views/InApp/index.tsx
@@ -1,9 +1,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useNavigation } from '@react-navigation/native';
+import { Button, Text } from '@ui-kitten/components';
+import { isDevice } from 'expo-device';
+import { authenticateAsync } from 'expo-local-authentication';
 import React from 'react';
 import { useStore } from 'react-redux';
 
 import TabNavigation from '../../components/TabNavigation';
+import ViewLayout from '../../components/ViewLayout';
 import tabsConfig from '../../config/tabs';
 
 const InApp: React.FC = () => {
@@ -13,9 +17,31 @@ const InApp: React.FC = () => {
   const { getState } = useStore();
   const { user } = getState();
   const { isInitialized } = user;
+  const [isAuthenticated, setAuthentication] = React.useState(false);
 
   // redirects to Wizard if initialization not done
   if (!isInitialized) navigate('WizardScreens');
+
+  async function authenticate() {
+    // early-termination in simulator
+    if (!isDevice) return;
+
+    try {
+      const { success } = await authenticateAsync();
+
+      setAuthentication(success);
+    } catch (error) {
+      throw new Error(`Error while using LocalAuthentication: ${error}`);
+    }
+  }
+
+  if (!isAuthenticated)
+    return (
+      <ViewLayout>
+        <Text>Locked</Text>
+        <Button onPress={() => authenticate()}>Authenticate</Button>
+      </ViewLayout>
+    );
 
   return (
     <Navigator
