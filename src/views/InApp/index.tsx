a7d2667382770d54d34a4b93f03e050a4419fbeb
Damien Senger <damien.senger@castoredc.com>
2020-04-12 10:21:17 +0200

    feat: trigger LocalAuthentication when app brought back from background

--- src/views/InApp/index.tsx
+++ src/views/InApp/index.tsx
@@ -4,6 +4,7 @@ import { Button, Text } from '@ui-kitten/components';
 import { isDevice } from 'expo-device';
 import { authenticateAsync } from 'expo-local-authentication';
 import React from 'react';
+import { AppState, AppStateStatus } from 'react-native';
 import { useStore } from 'react-redux';
 
 import TabNavigation from '../../components/TabNavigation';
@@ -18,13 +19,18 @@ const InApp: React.FC = () => {
   const { user } = getState();
   const { isInitialized } = user;
   const [isAuthenticated, setAuthentication] = React.useState(false);
+  const [appState, setAppState] = React.useState(AppState.currentState);
 
   // redirects to Wizard if initialization not done
   if (!isInitialized) navigate('WizardScreens');
 
+  /**
+   * Triggers the local authentication API of the device, in order to make sure
+   * that the user is the actual owner of the phone
+   */
   async function authenticate() {
     // early-termination in simulator
-    if (!isDevice) return;
+    if (!isDevice) return setAuthentication(true);
 
     try {
       const { success } = await authenticateAsync();
@@ -35,14 +41,40 @@ const InApp: React.FC = () => {
     }
   }
 
-  if (!isAuthenticated)
+  /**
+   * This effect aims to trigger the local authentication each time the app is
+   * brought back in the foreground after being in the background or inactive.
+   */
+  React.useEffect(() => {
+    function onAppStateChange(nextAppState: AppStateStatus): void {
+      switch (nextAppState) {
+        case 'active':
+          if (appState === 'background') setAuthentication(false);
+          break;
+
+        default:
+          setAppState(nextAppState);
+      }
+    }
+
+    AppState.addEventListener('change', onAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', onAppStateChange);
+    };
+  }, [appState]);
+
+  // Authetication screen displayed when the application is locked
+  if (!isAuthenticated) {
     return (
       <ViewLayout>
         <Text>Locked</Text>
         <Button onPress={() => authenticate()}>Authenticate</Button>
       </ViewLayout>
     );
+  }
 
+  // Bottom tab navigator rendered when the application is unlocked
   return (
     <Navigator
       initialRouteName="DashboardScreens"
